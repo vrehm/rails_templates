02dd239a1d0b0391eb42adcfed2b4efba4cdaae5
Vincent REHM <vincentrehm@yahoo.fr>
2018-10-14 17:20:01 +0200

    next homepage layout

--- template.rb
+++ template.rb
@@ -184,6 +184,8 @@ Read more at: https://github.com/plataformatec/devise
 }
 end
 
+rails_command 'db:seed'
+
 ##############################
 ## APPLYING CUSTOM FILES
 ##############################
@@ -193,6 +195,7 @@ worker: bundle exec sidekiq -C config/sidekiq.yml
 }
 end
 
+run 'rm config/routes.rb'
 create_file 'config/routes.rb' do
   %{Rails.application.routes.draw do
   devise_for :users
@@ -203,102 +206,24 @@ end
 }
 end
 
+run 'rm app/views/home/show.html.erb'
 create_file 'app/views/home/show.html.erb' do
-  %{<div class="ui fixed inverted menu">
-  <div class="ui container">
-    <a href="#" class="header item">
-      <img class="logo" src="https://semantic-ui.com/examples/assets/images/logo.png">
-      Project Name
-    </a>
-    <a href="#" class="item">Home</a>
-    <div class="ui simple dropdown item">
-      Dropdown <i class="dropdown icon"></i>
-      <div class="menu">
-        <a class="item" href="#">Link Item</a>
-        <a class="item" href="#">Link Item</a>
-        <div class="divider"></div>
-        <div class="header">Header Item</div>
-        <div class="item">
-          <i class="dropdown icon"></i>
-          Sub Menu
-          <div class="menu">
-            <a class="item" href="#">Link Item</a>
-            <a class="item" href="#">Link Item</a>
-          </div>
-        </div>
-        <a class="item" href="#">Link Item</a>
-      </div>
-    </div>
+  %{<div class="ui raised very padded text container segment">
+    <h2 class="ui header">Dogs Roles with Humans</h2>
+    <p>Domestic dogs inherited complex behaviors, such as bite inhibition, from their wolf ancestors, which would have been pack hunters with complex body language. These sophisticated forms of social cognition and communication may account for their trainability, playfulness, and ability to fit into human households and social situations, and these attributes have given dogs a relationship with humans that has enabled them to become one of the most successful species on the planet today.</p>
+    <p>The dogs' value to early human hunter-gatherers led to them quickly becoming ubiquitous across world cultures. Dogs perform many roles for people, such as hunting, herding, pulling loads, protection, assisting police and military, companionship, and, more recently, aiding handicapped individuals. This impact on human society has given them the nickname "man's best friend" in the Western world. In some cultures, however, dogs are also a source of meat.</p>
   </div>
-</div>
-
-<div class="ui main text container">
-  <h1 class="ui header">Semantic UI Fixed Template</h1>
-  <p>This is a basic fixed menu template using fixed size containers.</p>
-  <p>A text container is used for the main container, which is useful for single column layouts</p>
-  <img class="wireframe" src="https://semantic-ui.com/examples/assets/images/wireframe/media-paragraph.png">
-  <img class="wireframe" src="https://semantic-ui.com/examples/assets/images/wireframe/paragraph.png">
-  <img class="wireframe" src="https://semantic-ui.com/examples/assets/images/wireframe/paragraph.png">
-  <img class="wireframe" src="https://semantic-ui.com/examples/assets/images/wireframe/paragraph.png">
-  <img class="wireframe" src="https://semantic-ui.com/examples/assets/images/wireframe/paragraph.png">
-  <img class="wireframe" src="https://semantic-ui.com/examples/assets/images/wireframe/paragraph.png">
-  <img class="wireframe" src="https://semantic-ui.com/examples/assets/images/wireframe/paragraph.png">
-</div>
-
-<div class="ui inverted vertical footer segment">
-  <div class="ui center aligned container">
-    <div class="ui stackable inverted divided grid">
-      <div class="three wide column">
-        <h4 class="ui inverted header">Group 1</h4>
-        <div class="ui inverted link list">
-          <a href="#" class="item">Link One</a>
-          <a href="#" class="item">Link Two</a>
-          <a href="#" class="item">Link Three</a>
-          <a href="#" class="item">Link Four</a>
-        </div>
-      </div>
-      <div class="three wide column">
-        <h4 class="ui inverted header">Group 2</h4>
-        <div class="ui inverted link list">
-          <a href="#" class="item">Link One</a>
-          <a href="#" class="item">Link Two</a>
-          <a href="#" class="item">Link Three</a>
-          <a href="#" class="item">Link Four</a>
-        </div>
-      </div>
-      <div class="three wide column">
-        <h4 class="ui inverted header">Group 3</h4>
-        <div class="ui inverted link list">
-          <a href="#" class="item">Link One</a>
-          <a href="#" class="item">Link Two</a>
-          <a href="#" class="item">Link Three</a>
-          <a href="#" class="item">Link Four</a>
-        </div>
-      </div>
-      <div class="seven wide column">
-        <h4 class="ui inverted header">Footer Header</h4>
-        <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-      </div>
-    </div>
-    <div class="ui inverted section divider"></div>
-    <img src="https://semantic-ui.com/examples/assets/images/logo.png" class="ui centered mini image">
-    <div class="ui horizontal inverted small divided link list">
-      <a class="item" href="#">Site Map</a>
-      <a class="item" href="#">Contact Us</a>
-      <a class="item" href="#">Terms and Conditions</a>
-      <a class="item" href="#">Privacy Policy</a>
-    </div>
-  </div>
-</div>
 
 }
 end
 
-create_file 'app/assets/stylesheets/application.css.scss' do
+run 'rm app/assets/stylesheets/application.css'
+create_file 'app/assets/stylesheets/application.scss' do
   %{@import "semantic-ui";
 }
 end
 
+run 'rm app/assets/javascripts/application.js'
 create_file 'app/assets/javascripts/application.js' do
   %{// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
@@ -322,33 +247,36 @@ create_file 'app/assets/javascripts/application.js' do
 }
 end
 
-# create_file 'app/views/layouts/application.html.erb' do
-#   %{<!DOCTYPE html>
-# <html>
-#   <head>
-#     <!-- Standard Meta -->
-#     <meta charset="utf-8" />
-#     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-#     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+run 'rm app/views/layouts/application.html.erb'
+create_file 'app/views/layouts/application.html.erb' do
+  %{<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Standard Meta -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-#     <!-- Site Properties -->
-#     <title><%= yield(:title) %></title>
-#     <%= csrf_meta_tags %>
-#     <%= csp_meta_tag %>
+    <!-- Site Properties -->
+    <title><%= Rails.application.class.parent_name %></title>
+    <%= csrf_meta_tags %>
+    <%= csp_meta_tag %>
 
-#     <%= stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track': 'reload' %>
-#     <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>
-#   </head>
+    <!-- Site CSS -->
+    <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+  </head>
 
-#   <body>
-#     <%= yield %>
-#   </body>
-# </html>
-# }
-#end
+  <body>
+    <%= yield %>
+    <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>
+  </body>
+</html>
+}
+end
 
 after_bundle do
   rails_command 'db:migrate'
+  rails_command 'assets:precompile'
   git add: '.'
   git commit: "-m 'Initial commit'"
 end
