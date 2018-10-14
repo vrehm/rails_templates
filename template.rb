7b1051052f4895f41d34ce8ce52b77aa25d22215
Vincent REHM <vincentrehm@yahoo.fr>
2018-10-14 17:35:16 +0200

    message loading ui

--- template.rb
+++ template.rb
@@ -215,6 +215,15 @@ create_file 'app/views/home/show.html.erb' do
     <p>Domestic dogs inherited complex behaviors, such as bite inhibition, from their wolf ancestors, which would have been pack hunters with complex body language. These sophisticated forms of social cognition and communication may account for their trainability, playfulness, and ability to fit into human households and social situations, and these attributes have given dogs a relationship with humans that has enabled them to become one of the most successful species on the planet today.</p>
     <p>The dogs' value to early human hunter-gatherers led to them quickly becoming ubiquitous across world cultures. Dogs perform many roles for people, such as hunting, herding, pulling loads, protection, assisting police and military, companionship, and, more recently, aiding handicapped individuals. This impact on human society has given them the nickname "man's best friend" in the Western world. In some cultures, however, dogs are also a source of meat.</p>
   </div>
+  <div class="ui icon message">
+    <i class="notched circle loading icon"></i>
+    <div class="content">
+      <div class="header">
+        Just one second
+      </div>
+      <p>We're fetching that content for you.</p>
+    </div>
+  </div>
 
 }
 end
@@ -270,6 +279,7 @@ create_file 'app/views/layouts/application.html.erb' do
 
   <body>
     <%= yield %>
+    <!-- Site JS -->
     <%= javascript_include_tag 'application',
       'data-turbolinks-track': 'reload',
       'data-turbolinks-suppress-warning': 'true',
