16b3273fa2145e713c957fec092aa7152d77b1a6
Vincent REHM <vincentrehm@yahoo.fr>
2018-10-14 17:48:11 +0200

    padded container

--- template.rb
+++ template.rb
@@ -215,7 +215,7 @@ create_file 'app/views/home/show.html.erb' do
   <p>Domestic dogs inherited complex behaviors, such as bite inhibition, from their wolf ancestors, which would have been pack hunters with complex body language. These sophisticated forms of social cognition and communication may account for their trainability, playfulness, and ability to fit into human households and social situations, and these attributes have given dogs a relationship with humans that has enabled them to become one of the most successful species on the planet today.</p>
   <p>The dogs' value to early human hunter-gatherers led to them quickly becoming ubiquitous across world cultures. Dogs perform many roles for people, such as hunting, herding, pulling loads, protection, assisting police and military, companionship, and, more recently, aiding handicapped individuals. This impact on human society has given them the nickname "man's best friend" in the Western world. In some cultures, however, dogs are also a source of meat.</p>
 </div>
-<div class="container">
+<div class="ui padded text container">
   <div class="ui icon message">
     <i class="notched circle loading icon"></i>
     <div class="content">
