25bad5f714f7341755c8836f5af6d9042780a7d3
Vincent REHM <vincentrehm@yahoo.fr>
2018-10-14 15:52:33 +0200

    improving layout

--- template.rb
+++ template.rb
@@ -25,6 +25,7 @@ gem_group :development, :test do
   gem 'pry-rails'
   gem 'rspec-rails', '~> 3.7'
   gem 'pry-byebug'
+  gem 'foreman'
 end
 
 # Remove all coments and empty space fron the gemfile
@@ -193,12 +194,165 @@ worker: bundle exec sidekiq -C config/sidekiq.yml
 }
 end
 
+create_file 'config/routes.rb' do
+  %{Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#show'
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+end
+}
+end
+
+create_file 'app/views/home/show.html.erb' do
+  %{<div class="ui fixed inverted menu">
+  <div class="ui container">
+    <a href="#" class="header item">
+      <img class="logo" src="assets/images/logo.png">
+      Project Name
+    </a>
+    <a href="#" class="item">Home</a>
+    <div class="ui simple dropdown item">
+      Dropdown <i class="dropdown icon"></i>
+      <div class="menu">
+        <a class="item" href="#">Link Item</a>
+        <a class="item" href="#">Link Item</a>
+        <div class="divider"></div>
+        <div class="header">Header Item</div>
+        <div class="item">
+          <i class="dropdown icon"></i>
+          Sub Menu
+          <div class="menu">
+            <a class="item" href="#">Link Item</a>
+            <a class="item" href="#">Link Item</a>
+          </div>
+        </div>
+        <a class="item" href="#">Link Item</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="ui main text container">
+  <h1 class="ui header">Semantic UI Fixed Template</h1>
+  <p>This is a basic fixed menu template using fixed size containers.</p>
+  <p>A text container is used for the main container, which is useful for single column layouts</p>
+  <img class="wireframe" src="assets/images/wireframe/media-paragraph.png">
+  <img class="wireframe" src="assets/images/wireframe/paragraph.png">
+  <img class="wireframe" src="assets/images/wireframe/paragraph.png">
+  <img class="wireframe" src="assets/images/wireframe/paragraph.png">
+  <img class="wireframe" src="assets/images/wireframe/paragraph.png">
+  <img class="wireframe" src="assets/images/wireframe/paragraph.png">
+  <img class="wireframe" src="assets/images/wireframe/paragraph.png">
+</div>
+
+<div class="ui inverted vertical footer segment">
+  <div class="ui center aligned container">
+    <div class="ui stackable inverted divided grid">
+      <div class="three wide column">
+        <h4 class="ui inverted header">Group 1</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Link One</a>
+          <a href="#" class="item">Link Two</a>
+          <a href="#" class="item">Link Three</a>
+          <a href="#" class="item">Link Four</a>
+        </div>
+      </div>
+      <div class="three wide column">
+        <h4 class="ui inverted header">Group 2</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Link One</a>
+          <a href="#" class="item">Link Two</a>
+          <a href="#" class="item">Link Three</a>
+          <a href="#" class="item">Link Four</a>
+        </div>
+      </div>
+      <div class="three wide column">
+        <h4 class="ui inverted header">Group 3</h4>
+        <div class="ui inverted link list">
+          <a href="#" class="item">Link One</a>
+          <a href="#" class="item">Link Two</a>
+          <a href="#" class="item">Link Three</a>
+          <a href="#" class="item">Link Four</a>
+        </div>
+      </div>
+      <div class="seven wide column">
+        <h4 class="ui inverted header">Footer Header</h4>
+        <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+      </div>
+    </div>
+    <div class="ui inverted section divider"></div>
+    <img src="assets/images/logo.png" class="ui centered mini image">
+    <div class="ui horizontal inverted small divided link list">
+      <a class="item" href="#">Site Map</a>
+      <a class="item" href="#">Contact Us</a>
+      <a class="item" href="#">Terms and Conditions</a>
+      <a class="item" href="#">Privacy Policy</a>
+    </div>
+  </div>
+</div>
+
+}
+end
+
+create_file 'app/assets/stylesheets/application.css.scss' do
+  %{@import "semantic-ui";
+}
+end
+
+create_file 'app/assets/javascripts/application.js' do
+  %{// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+// Loads all Semantic javascripts
+//= require semantic-ui
+//= require_tree .
+}
+end
+
+create_file 'app/views/layouts/application.html.erb' do
+  %{<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <!-- Site Properties -->
+    <title><%= yield(:title) %></title>
+    <%= csrf_meta_tags %>
+    <%= csp_meta_tag %>
+
+    <%= stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+    <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>
+  </head>
+
+  <body>
+    <%= yield %>
+  </body>
+</html>
+}
+end
+
 after_bundle do
-  rails_command 'g semantic:install'
   rails_command 'db:migrate'
   git add: '.'
   git commit: "-m 'Initial commit'"
 end
 
 after_bundle do
+  run 'foreman start'
 end
